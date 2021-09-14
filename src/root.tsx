3ed1703a9e84011d8e4151a13d59949aab3a0be9
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-14 18:05:44 -0300

    Inserindo o estilizo global à aplicação

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles/theme'
+import { GlobalStyle } from 'styles/globals'
 import { App } from './app'
 
 export const Root = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   )
