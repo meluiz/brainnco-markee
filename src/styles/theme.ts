142f0e37da7782aa7da614a3a14d669294c5d828
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-14 17:54:05 -0300

    Aplicando a tipagem no tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    black: '#1e293b',
+    lightBlack: '#293445',
+    gray: '#e4e5e7',
+    white: '#fafafa',
+    primary: '#1fc8e1',
+  },
+}
