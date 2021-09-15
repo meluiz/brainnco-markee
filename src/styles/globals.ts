c328fb6a36eb300e68e7e5c5a66e0c58d8290d00
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-15 10:23:30 -0300

    Ajustando o CSS ao Reset CSS

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -12,14 +12,6 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html {
-    font-size: 62.5%;
-  }
-
-  body {
-    font-family: 'DM Sans', Arial, Helvetica, sans-serif;
-  }
-
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -36,9 +28,9 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     border: 0;
-    font-size: 100%;
     font: inherit;
     vertical-align: baseline;
+    font-family: 'DM Sans', Arial, Helvetica, sans-serif;
   }
 
   :focus {
@@ -114,9 +106,9 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-size: 100%;
-    -webkit-text-size-adjust: 100%;
-    -ms-text-size-adjust: 100%;
+    font-size: 62.5%;
+    -webkit-text-size-adjust: 62.5%;
+    -ms-text-size-adjust: 62.5%;
   }
 
   a:focus {
@@ -158,7 +150,6 @@ export const GlobalStyle = createGlobalStyle`
   input,
   select,
   textarea {
-    font-size: 100%;
     margin: 0;
     vertical-align: baseline;
     *vertical-align: middle;
