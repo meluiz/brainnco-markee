386f8ff252bd11cc351fbf26566e5e4004eb3e4c
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-15 16:56:40 -0300

    Adicionando os componentes Content e Sidebar

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,16 @@
+/* ------| Componentes |------ */
+import { Content } from 'components/Content'
+import { Sidebar } from 'components/sidebar'
+
+/* ------| Estilos |------ */
 import { Container, Wrapper } from 'styles/layout'
 
 export const App = () => {
   return (
     <Wrapper>
       <Container>
-        Olá
+        <Sidebar />
+        <Content />
       </Container>
     </Wrapper>
   )
