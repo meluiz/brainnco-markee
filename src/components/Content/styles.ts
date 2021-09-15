981173fcad3a615cbd638a02285ff1febcbf2fca
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-15 16:58:28 -0300

    Estilizando o componente principal

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,1 +1,10 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.main`${({ theme }) => css`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  position: relative;
+  background-color: ${theme.colors.white};
+`}`
--- src/components/sidebar/styles.ts
+++ src/components/sidebar/styles.ts
@@ -1,1 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.aside`${({ theme }) => css`
+  width: 332px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  flex-grow: 0;
+  position: relative;
+  background-color: ${theme.colors.black};
+`}`
