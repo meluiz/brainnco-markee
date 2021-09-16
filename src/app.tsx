ad5fc920872f4e359b737a3b4a917ba659638e26
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-16 17:47:06 -0300

    Tipando os arquivos

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,15 @@ import { Sidebar } from 'components/sidebar'
 /* ------| Estilos |------ */
 import { Container, Wrapper } from 'styles/layout'
 
+/* ------| Tipagem |------ */
+export type FileType = {
+  id: string
+  name: string
+  content: string
+  active: boolean
+  status: 'none' | 'editing' | 'saving' | 'saved'
+}
+
 export const App = () => {
   return (
     <Wrapper>
