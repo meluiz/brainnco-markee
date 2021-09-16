fdded3f6aed02880731e8425b21a4e1e3ee94e41
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-16 17:43:36 -0300

    Mapeando os arquivos

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -53,62 +53,33 @@ export const Sidebar = () => {
           </Button>
         </NavigationActions>
         <NavigationList>
-          <NavigationListItem>
-            <NavigationListLink href='/#'>
-              <NavigationListLinkFilename>
-                <div className='icon'>
-                  <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M14 2.88062V8.88062H20' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M16 13.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M16 17.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M10 9.88062H9H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                  </svg>
-                </div>
+          {files && files.map((file) => (
+            <NavigationListItem key={file.id}>
+              <NavigationListLink href='/#'>
+                <NavigationListLinkFilename>
+                  <div className='icon'>
+                    <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M14 2.88062V8.88062H20' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M16 13.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M16 17.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M10 9.88062H9H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
+                    </svg>
+                  </div>
                 <span className='text'>README.md</span>
-              </NavigationListLinkFilename>
-            </NavigationListLink>
-          </NavigationListItem>
-          <NavigationListItem>
-            <NavigationListLink href='/#'>
-              <NavigationListLinkFilename>
-                <div className='icon'>
-                  <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M14 2.88062V8.88062H20' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M16 13.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M16 17.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M10 9.88062H9H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                  </svg>
-                </div>
-                <span className='text'>README.md</span>
-              </NavigationListLinkFilename>
-            </NavigationListLink>
-          </NavigationListItem>
-          <NavigationListItem>
-            <NavigationListLink href='/#'>
-              <NavigationListLinkFilename>
-                <div className='icon'>
-                  <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M14 2.88062V8.88062H20' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M16 13.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M16 17.8806H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                    <path d='M10 9.88062H9H8' stroke='currentColor' strokeOpacity='0.65' strokeLinecap='round' strokeLinejoin='round' />
-                  </svg>
-                </div>
-                <span className='text'>Content.md</span>
-              </NavigationListLinkFilename>
-              <NavigationListLinkStats>
-                <Button styleType='none'>
-                  <svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M1.5 0.880615L9.18 8.56061M9.5 0.880615L1.82 8.56061' stroke='white' strokeWidth='1.5' strokeLinecap='round' />
-                  </svg>
-                </Button>
-                <FileStats stats='none' />
-              </NavigationListLinkStats>
-            </NavigationListLink>
-          </NavigationListItem>
+                  <span className='text'>{file.name}</span>
+                </NavigationListLinkFilename>
+                <NavigationListLinkStats>
+                  <Button styleType='none'>
+                    <svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M1.5 0.880615L9.18 8.56061M9.5 0.880615L1.82 8.56061' stroke='white' strokeWidth='1.5' strokeLinecap='round' />
+                    </svg>
+                  </Button>
+                  <FileStats stats={file.status} />
+                </NavigationListLinkStats>
+              </NavigationListLink>
+            </NavigationListItem>
+          ))}
         </NavigationList>
       </Navigation>
     </Wrapper>
