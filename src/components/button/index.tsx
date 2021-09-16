9750771e2a11d9f7da00ffa746130aae92d286f5
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-16 13:03:47 -0300

    Tipando e adicionando novas props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,11 +6,14 @@ import { Btn } from './styles'
 /* ------| Tipagem |------ */
 type ButtonType = {
   children?: ReactNode
+  type?: 'button' | 'submit' | 'reset'
+  styleType: 'primary' | 'none'
+  onClick?: () => void
 }
 
-export const Button = ({ children }: ButtonType) => {
+export const Button = ({ children, type = 'button', styleType, onClick }: ButtonType) => {
   return (
-    <Btn>
+    <Btn styleType={styleType} type={type} onClick={onClick}>
       {children}
     </Btn>
   )
