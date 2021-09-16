1131f014914df60fbb758c8905e573d63fc80396
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-16 17:48:33 -0300

    Simulando um estado com dados dos arquivos

--- src/app.tsx
+++ src/app.tsx
@@ -17,6 +17,36 @@ export type FileType = {
 }
 
 export const App = () => {
+  const [files, setFiles] = useState<FileType[]>([
+    {
+      id: '11d6b24b-a886-43e1-8b8f-bfd6d035ea96',
+      name: 'None.md',
+      content: '',
+      active: false,
+      status: 'none',
+    },
+    {
+      id: '2844bfce-4eac-46cd-88e7-a51e5bb25bc1',
+      name: 'Editing.md',
+      content: '',
+      active: false,
+      status: 'editing',
+    },
+    {
+      id: 'ed0c3bed-d946-4d96-ad3f-d1d57db65181',
+      name: 'Saving.md',
+      content: '',
+      active: false,
+      status: 'saving',
+    },
+    {
+      id: 'f8f569ad-dffd-4a37-b9c0-e89bc669a593',
+      name: 'Saved.md',
+      content: '',
+      active: false,
+      status: 'saved',
+    },
+  ])
   return (
     <Wrapper>
       <Container>
