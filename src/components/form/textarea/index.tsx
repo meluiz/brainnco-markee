ae945e33a76bb16a23e056391cada1fc14147814
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-17 01:31:31 -0300

    Removendo children. Valor default ao textarea

--- src/components/form/textarea/index.tsx
+++ src/components/form/textarea/index.tsx
@@ -1,16 +1,13 @@
-import { ReactNode } from 'react'
-
 /* ------| Estilos |------ */
 import { Area } from './styled'
 
 /* ------| Tipagem |------ */
 type TextareaType = {
-  children?: ReactNode
   resize?: 'none' | 'both' | 'horizontal' | 'vertical' | 'block' | 'inline'
 }
 
-export const Textarea = ({ children, resize }: TextareaType) => {
+export const Textarea = ({ resize }: TextareaType) => {
   return (
-    <Area resize={resize}>{children}</Area>
+    <Area resize={resize} defaultValue='## Bootcamp Brainn Co.' />
   )
 }
