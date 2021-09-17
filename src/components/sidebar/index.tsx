18cb27535f9566591d5ae9d464eda8568e127aca
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-16 22:41:09 -0300

    Adicionando todo

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -74,6 +74,7 @@ export const Sidebar = ({ files }: SidebarType) => {
                   </div>
                   <span className='text'>{file.name}</span>
                 </NavigationListLinkFilename>
+                {/* TODO: Mostrar status somente quando o arquivo estiver ativo */}
                 <NavigationListLinkStats>
                   <Button styleType='none'>
                     <svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
