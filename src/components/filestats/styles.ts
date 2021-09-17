aa668ba38d993031a0b4abc7d521c27b5a3977d5
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-17 16:06:00 -0300

    Removendo estilos dos ícones

--- src/components/filestats/styles.ts
+++ src/components/filestats/styles.ts
@@ -18,30 +18,3 @@ export const Editing = styled.span`${({ theme }) => css`
   border-radius: 999px;
   background-color: ${theme.colors.primary};
 `}`
-
-export const Saving = styled.svg`
-  animation: spin 600ms linear infinite;
-
-  @keyframes spin {
-    from { transform: rotate(0deg) }
-    to { transform: rotate(360deg) }
-  }
-`
-
-export const Saved = styled.svg`
-  animation: saved 600ms ease forwards;
-  stroke-dasharray: 25;
-  stroke-dashoffset: -25;
-  
-  @keyframes saved {
-    from {
-      stroke-dasharray: 25;
-      stroke-dashoffset: -25;
-    }
-
-    to {
-      stroke-dasharray: 25;
-      stroke-dashoffset: 0;
-    }
-  }
-`
