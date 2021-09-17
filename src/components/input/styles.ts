259e514d54218b88ee526e2cba52e0e98e14d895
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-17 00:40:59 -0300

    Definindo o estilo padrão do Input

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,11 +1,10 @@
 import styled, { css } from 'styled-components'
 
 export const Inpt = styled.input`${({ theme }) => css`
-  width: 100%;
+  min-width: 112px;
   min-height: 40px;
   border: 0;
-  font-size: 1.8rem;
-  font-weight: 700;
+  font-size: 1.6rem;
   background-color: transparent;
   color: ${theme.colors.black};
   font-family: 'DM Sans', Arial, Helvetica, sans-serif;
