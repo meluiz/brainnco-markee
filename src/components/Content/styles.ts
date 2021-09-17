a5bc0668f4ca5c92e14812c7c4cf7f8e3ebda8a6
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-17 00:38:56 -0300

    Refatorando o conteúdo

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,22 +1,26 @@
 import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.main`${({ theme }) => css`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
+  grid-area: Content;
   position: relative;
   background-color: ${theme.colors.white};
   overflow: hidden;
+  overflow-y: auto;
+
+  scrollbar-width: thin;
+  scrollbar-color: #c9c8c7 #f1f1f1;
+
+  ::-webkit-scrollbar { width: 10px }
+  ::-webkit-scrollbar-track { background: rgba(0,0,0,.06) }
+  ::-webkit-scrollbar-thumb { background: rgba(0,0,0,.1) }
+  ::-webkit-scrollbar-thumb:hover { background: rgba(0,0,0,.2) }
 `}`
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  margin: 0 auto;
+  min-height: 100%;
+  display: block;
+  position: relative;
   padding: 8px 24px;
 `
 
@@ -24,12 +28,39 @@ export const Header = styled.header`
   width: 100%;
   display: flex;
   align-items: center;
-  gap: 16px;
+  gap: 8px;
+  position: relative;
+  padding: 12px 0;
+
+  input {
+    width: 100%;
+    font-size: 1.8rem;
+    font-weight: 700;
+  }
+`
+export const HeaderIcon = styled.div`${({ theme }) => css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${theme.colors.primary};
+`}`
+
+export const MarkdownEditor = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 96px);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: 'Editor Preview';
   padding: 12px 0;
 `
 
-export const HeaderIcon = styled.div`${({ theme }) => css`
-  flex-grow: 0;
-  flex-shrink: 0;
-  color: ${theme.colors.primary};
-`}`
+export const Editor = styled.div`
+  grid-area: Editor;
+  padding-right: 12px;
+  border-right: 1px solid rgba(30, 41, 59, 0.12);
+  `
+
+export const Preview = styled.div`
+  padding-left: 12px;
+  grid-area: Preview;
+`
