a9d683c2cb5b5201fb8e1d6249685685d3dac745
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-16 23:15:00 -0300

    Estilizando input

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,1 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const Inpt = styled.input`${({ theme }) => css`
+  width: 100%;
+  min-height: 40px;
+  border: 0;
+  font-size: 1.8rem;
+  font-weight: 700;
+  background-color: transparent;
+  color: ${theme.colors.black};
+  font-family: 'DM Sans', Arial, Helvetica, sans-serif;
+`}`
