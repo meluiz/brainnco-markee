2fd1ca3bdd42f62ef9d66ef946dea517955886c6
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-18 20:30:22 -0300

    Tipando o onChange

--- src/components/form/textarea/index.tsx
+++ src/components/form/textarea/index.tsx
@@ -1,4 +1,4 @@
-import { RefObject } from 'react'
+import { ChangeEvent, RefObject } from 'react'
 
 /* ------| Estilos |------ */
 import { Area } from './styled'
@@ -6,6 +6,7 @@ import { Area } from './styled'
 /* ------| Tipagem |------ */
 type TextareaType = {
   reference?: RefObject<HTMLTextAreaElement>
+  onChange?: (event?: ChangeEvent<HTMLTextAreaElement>) => void
   resize?: 'none' | 'both' | 'horizontal' | 'vertical' | 'block' | 'inline'
 }
 
