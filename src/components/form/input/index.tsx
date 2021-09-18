c81886bf19f1c6bb40d74c77c3f1e535a31bb8c8
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-18 19:56:06 -0300

    Tipando reference e onChange

--- src/components/form/input/index.tsx
+++ src/components/form/input/index.tsx
@@ -1,4 +1,5 @@
 /* ------| Estilos |------ */
+import { ChangeEvent, RefObject } from 'react'
 import { Inpt } from './styles'
 
 /* ------| Tipagem |------ */
@@ -7,6 +8,8 @@ type InputType = {
   name?: string
   value?: string
   placeholder?: string
+  onChange?: (event?: ChangeEvent<HTMLInputElement>) => void
+  reference?: RefObject<HTMLInputElement>
   type: 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' |
         'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' |
         'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' |
