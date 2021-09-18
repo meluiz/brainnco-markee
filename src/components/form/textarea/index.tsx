839556b63752dd9adc42f7089ba4a9fb59fab006
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-18 19:02:39 -0300

    Propriedade para referência do textarea

--- src/components/form/textarea/index.tsx
+++ src/components/form/textarea/index.tsx
@@ -1,13 +1,16 @@
+import { RefObject } from 'react'
+
 /* ------| Estilos |------ */
 import { Area } from './styled'
 
 /* ------| Tipagem |------ */
 type TextareaType = {
+  reference?: RefObject<HTMLTextAreaElement>
   resize?: 'none' | 'both' | 'horizontal' | 'vertical' | 'block' | 'inline'
 }
 
-export const Textarea = ({ resize }: TextareaType) => {
+export const Textarea = ({ resize, reference }: TextareaType) => {
   return (
-    <Area resize={resize} defaultValue='## Bootcamp Brainn Co.' />
+    <Area ref={reference} resize={resize} defaultValue='## Bootcamp Brainn Co.' />
   )
 }
