2c08345b480870a137bf6b36f255e0d47698feb7
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-20 12:56:57 -0300

    Componente para oícone X

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -54,6 +54,25 @@ export const FileIcon = ({ size = 24 }: IconType) => {
   )
 }
 
+export const XIcon = ({ size = 24 }: IconType) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox='0 0 11 10'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M1.5 0.880615L9.18 8.56061M9.5 0.880615L1.82 8.56061'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+      />
+    </svg>
+  )
+}
+
 export const SavingIcon = ({ size = 24 }: IconType) => {
   return (
     <Saving
