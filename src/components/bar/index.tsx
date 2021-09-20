06277e3137fb2811b6e38bdad7de73d590ff735e
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-19 22:03:42 -0300

    Estrutura do bar

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -1,18 +1,43 @@
 /* ------| Medias |------- */
+import { Button } from 'components/button'
+import { MenuIcon } from 'components/icons'
 import LogoImage from 'media/img/logo.png'
+import { Dispatch, SetStateAction } from 'react'
 
 /* ------| Estilos |------- */
 import {
+  Column,
   Container,
-  LeftSide,
   Logo,
   LogoLink,
+  ToggleMenu,
+  Wrapper,
 } from './styles'
 
+/* ------| Tipagem |------- */
 export const Bar = () => {
   return (
-    <div>
-      Olá
-    </div>
+    <Wrapper>
+      <Container>
+        <Column>
+          <ToggleMenu>
+            <Button
+              styleType='none'
+              type='button'
+            >
+              <MenuIcon size={24} />
+            </Button>
+          </ToggleMenu>
+        </Column>
+        <Column posYItems='center' posXItems='center'>
+          <LogoLink href='/'>
+            <Logo>
+              <img src={LogoImage} alt='' />
+            </Logo>
+          </LogoLink>
+        </Column>
+        <Column />
+      </Container>
+    </Wrapper>
   )
 }
