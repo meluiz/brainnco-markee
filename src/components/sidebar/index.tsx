c4bdf54f8d656a5d7e63fa513046f1ad1d373f3e
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-20 12:57:39 -0300

    Subsituindo o ícone por componente

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,7 +3,7 @@ import { Dispatch, MouseEvent, SetStateAction } from 'react'
 /* ------| Componentes |------ */
 import { Button } from 'components/button'
 import { FileStats } from 'components/filestats'
-import { FileIcon } from 'components/icons'
+import { FileIcon, XIcon } from 'components/icons'
 
 /* ------| Estilos |------ */
 import {
@@ -109,9 +109,7 @@ export const Sidebar = ({
                     styleType='none'
                     onClick={(event) => event && handleDeleteFile(file.id, event)}
                   >
-                    <svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path d='M1.5 0.880615L9.18 8.56061M9.5 0.880615L1.82 8.56061' stroke='white' strokeWidth='1.5' strokeLinecap='round' />
-                    </svg>
+                    <XIcon />
                   </Button>
                   {file.active && <FileStats stats={file.status} />}
                 </NavigationListLinkStats>
